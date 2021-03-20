b73a0151e4f2c569ffe3168be44de48c04395ede
SMMDS <80556564+SMMDS@users.noreply.github.com>
2021-03-20 20:07:18 -0300

    Delete sc directory

--- sc/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-const botao = menu.querySelector(".icone");
-
-botao.addEventListener("click", function(event){
-    event.preventDefault();
-    menu.classList.toggle("responsivo");
-});
